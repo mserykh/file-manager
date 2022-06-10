96bea2f39c351a3f2cbde3ba3e6f4c8e4b88de1f
Maria Serykh <marie.serykh@gmail.com>
2022-06-11 00:36:03 +0200

    feat: add `.exit` command and handling unknown commands, implement changing a current directory to a home directory on start

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 import readline from 'readline';
+import os from 'os';
 
 import { greetUser } from './src/greetUser.js';
 import { getUsername } from './src/getUsername.js';
 import { farewellUser } from './src/farewellUser.js';
+import { serCurrentDir } from './src/utils.js'
 
 const username = getUsername();
+const userHomeDir = os.homedir();
 
 const init = () => {
   greetUser(username);
+  try {
+    process.chdir(userHomeDir);
+    console.log(`You are currently in ${serCurrentDir()}`);
+  } catch (err) {
+    console.log('Operation failed');
+  }
 };
 
 const readLine = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
+});
+
+readLine.on('line', (input) => {
+  switch (input) {
+    case '.exit':
+      readLine.close();
+      break;
+    default:
+      console.log('Invalid input');
+  }
 });
 
 readLine.on('SIGINT', () => {
---
+++ src/utils.js
@@ -0,0 +1,3 @@
+export const serCurrentDir = () => {
+  return process.cwd();
+};
